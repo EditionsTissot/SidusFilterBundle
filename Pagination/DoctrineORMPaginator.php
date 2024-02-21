ef2a4de6b5df84233800fdf42f649a0570cf0600
rem42 <me@remy.ovh>
2024-02-21 15:13:45 +0100

    chore: upgrade some doctrine elements

--- Pagination/DoctrineORMPaginator.php
+++ Pagination/DoctrineORMPaginator.php
@@ -148,7 +148,7 @@ class DoctrineORMPaginator implements \Countable, \IteratorAggregate
             }
 
             $this->appendTreeWalker($whereInQuery, WhereInWalker::class);
-            $whereInQuery->setHint(WhereInWalker::HINT_PAGINATOR_ID_COUNT, count($ids));
+            $whereInQuery->setHint(WhereInWalker::HINT_PAGINATOR_HAS_IDS, count($ids));
             $whereInQuery->setFirstResult(null)->setMaxResults(null);
             $whereInQuery->setParameter(WhereInWalker::PAGINATOR_ID_ALIAS, $ids);
             $whereInQuery->setCacheable($this->query->isCacheable());
